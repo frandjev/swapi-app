812ac1a584933bc3aca750e268582a61a1b7e9b9
Franco Maidana <frandjev@gmail.com>
2025-04-27 12:04:01 -0300

    Init

---
+++ src/main/java/com/exam/swapi/SwapiAppApplication.java
@@ -0,0 +1,11 @@
+package com.exam.swapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwapiAppApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SwapiAppApplication.class, args);
+    }
+}
